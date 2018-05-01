6a9a87c91ad1786a16d0fd59ac83ef2d4b481ec5
Thomas Stewart <thomas@stewarts.org.uk>
2018-05-01 17:45:50 +0100

    2.13 added https urls, removed greasemonkey.scriptvals, fixed title pages

--- IMDBAge.user.js
+++ IMDBAge.user.js
@@ -1,5 +1,5 @@
-/*  IMDBAge v2.12 - Greasemonkey script to add actors ages to IMDB pages
-    Copyright (C) 2005-2014 Thomas Stewart <thomas@stewarts.org.uk>
+/*  IMDBAge v2.13 - Greasemonkey script to add actors ages to IMDB pages
+    Copyright (C) 2005-2018 Thomas Stewart <thomas@stewarts.org.uk>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -14,72 +14,62 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-Inspired in 2001, Created on 24/03/2005, Last Changed 26/06/2014
-Major bug fixes and improvements by Christopher J. Madsen
+    Inspired in 2001, Created on 24/03/2005, Last Changed 01/05/2018
+    Major bug fixes and improvements by Christopher J. Madsen
 
-This is a Greasemonkey user script, see http://www.greasespot.net/ and
-https://addons.mozilla.org/firefox/addon/748
+    This is a Greasemonkey user script, see http://www.greasespot.net/ and
+    https://addons.mozilla.org/firefox/addon/748
 
-New versions can be found either on my site, userscripts, greasyfork, or monkeyguts:
-http://www.stewarts.org.uk/tomsweb/IMDBAge
-http://userscripts.org/scripts/show/1060
-https://greasyfork.org/scripts/2798-imdbage
-https://monkeyguts.com/code.php?id=268
+    New versions can be found on the following sites:
+    http://www.stewarts.org.uk/tomsweb/IMDBAge
+    http://userscripts.org/scripts/show/1060 (dead)
+    https://greasyfork.org/scripts/2798-imdbage
+    https://monkeyguts.com/code.php?id=268 (dead)
+    https://openuserjs.org/scripts/thomas_d_stewart/IMDBAge
+    https://github.com/thomasdstewart/IMDBAge
 
-This script adds the age and other various info onto IMDB pages. Specifically
-it adds some details to actor or actresses pages. It adds their age, their
-Tropical Zodiac Sign and their Chinese Zodiac Sign. As well as adding how many
-years ago and how old they were when they made the listed films. It also adds
-how long a go a film was made on a film page.
+    This script adds the age and other various info onto IMDB pages.
+    Specifically it adds some details to actor or actresses pages. It adds
+    their age, their Tropical Zodiac Sign and their Chinese Zodiac Sign. As
+    well as adding how many years ago and how old they were when they made the
+    listed films. It also adds how long a go a film was made on a film page.
 
-This script is not abandoned, email thomas@stewarts.org.uk if it breaks.
+    This script is not abandoned, email thomas@stewarts.org.uk if it breaks.
 
-Changelog
-
-* 2.12 fixed adding ages to individual films and fixed old style
-* 2.11 fixed date grabbing again
-* 2.10 fixed date grabbing
-* 2.9 fixed adding year to title with many years
-* 2.8 old style working, fixed death day for new style, improved year grabbing
-* 2.7 added persistent config, changed namespace
-* 2.6 fixed star signs and added unicode symbols
-* 2.5 fixed imdb updates
-* 2.4 fixed imdb updates
-* 2.3 improved year grabbing
-* 2.2 updated imdb text info, formatting, added ages to individual films
-* 2.1 Major changes, added signs, added config
-* 1.6 Added improvement ideas from Christopher J. Madsen, Added first imdb text files search, Reformatting
-* 1.5 Removed function enclosing while script
-* 1.3 First public version
-
-
-Any of the above can be turned on or off by setting the following settings in about:config:
-greasemonkey.scriptvals.http://www.stewarts.org.uk/IMDBAge.doFilmAge
-greasemonkey.scriptvals.http://www.stewarts.org.uk/IMDBAge.doNameAge
-greasemonkey.scriptvals.http://www.stewarts.org.uk/IMDBAge.doNameAges
-greasemonkey.scriptvals.http://www.stewarts.org.uk/IMDBAge.doSigns
+    Changelog
+    * 2.13 added https urls, removed scriptvals, fixed title pages
+    * 2.12 fixed adding ages to individual films and fixed old style
+    * 2.11 fixed date grabbing again
+    * 2.10 fixed date grabbing
+    * 2.9 fixed adding year to title with many years
+    * 2.8 old style working, fixed death day for new style, improved year grabbing
+    * 2.7 added persistent config, changed namespace
+    * 2.6 fixed star signs and added unicode symbols
+    * 2.5 fixed imdb updates
+    * 2.4 fixed imdb updates
+    * 2.3 improved year grabbing
+    * 2.2 updated imdb text info, formatting, added ages to individual films
+    * 2.1 Major changes, added signs, added config
+    * 1.6 Added improvement ideas from Christopher J. Madsen, Added first imdb text files search, Reformatting
+    * 1.5 Removed function enclosing while script
+    * 1.3 First public version
 */
 
-var doNameAge  = GM_getValue("doNameAge",  true);
-var doNameAges = GM_getValue("doNameAges", true);
-var doSigns    = GM_getValue("doSigns",    true);
-var doFilmAge  = GM_getValue("doFilmAge",  true);
-
-GM_setValue("doNameAge",  doNameAge)
-GM_setValue("doNameAges", doNameAges)
-GM_setValue("doSigns",    doSigns)
-GM_setValue("doFilmAge",  doFilmAge)
+var doNameAge  = true;
+var doNameAges = true;
+var doSigns    = true;
+var doFilmAge  = true;
 
 // ==UserScript==
 // @name        IMDBAge
 // @description Adds the age and other various info onto IMDB pages.
-// @version     2.12
+// @version     2.13
+// @author      Thomas Stewart
 // @namespace   http://www.stewarts.org.uk
-// @include     http://*imdb.com/name/*
-// @include     http://*imdb.com/title/*
-// @grant       GM_getValue
-// @grant       GM_setValue
+// @include     http*://*imdb.com/name/*
+// @include     http*://*imdb.com/title/*
 // @icon        http://www.stewarts.org.uk/tomsweb/IMDBAge?action=AttachFile&do=get&target=icon.png  
+// @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // ==/UserScript==
 
 /*
@@ -87,8 +77,8 @@ GM_setValue("doFilmAge",  doFilmAge)
 Title Test Cases:
 plain year              http://www.imdb.com/title/tt0056172/
 year range              http://www.imdb.com/title/tt0108757/
-year range still open   http://www.imdb.com/title/tt0804503/
-year with version       http://www.imdb.com/title/tt1008690/
+year range still open   http://www.imdb.com/title/tt0944947/
+year with version       http://www.imdb.com/title/tt1008690/ (dead)
 
 Name Test Cases: (In full for completeness)
 Born 18C -> Died 18C    http://us.imdb.com/name/nm1038177/ (Laurence Sterne 54)
@@ -97,22 +87,22 @@ Born 18C -> Died 20C    None
 Born 18C -> Died 21C    None
 Born 18C -> Alive       None
 
-Born 19C -> Died 19C    http://us.imdb.com/name/nm0786564/ (Anna Sewell 58)
-Born 19C -> Died 20C    http://us.imdb.com/name/nm0186440/ (Ward Crane 38)
-Born 19C -> Died 21C    http://us.imdb.com/name/nm0041807/ (Germaine Auger 112)
-Born 19C -> Alive       http://us.imdb.com/name/nm0008724/ (Dawlad Abiad 118)
+Born 19C -> Died 19C    https://us.imdb.com/name/nm0786564/ (Anna Sewell 58)
+Born 19C -> Died 20C    https://us.imdb.com/name/nm0186440/ (Ward Crane 38)
+Born 19C -> Died 21C    https://us.imdb.com/name/nm0041807/ (Germaine Auger 112)
+Born 19C -> Alive       https://us.imdb.com/name/nm0008724/ (Dawlad Abiad 118)
 
-Born 20C -> Died 20C    http://us.imdb.com/name/nm0001006/ (John Candy 43)
-Born 20C -> Died 21C    http://us.imdb.com/name/nm0670239/ (John Peel 65)
-Born 20C -> Alive       http://us.imdb.com/name/nm0088127/ (Alexis Bledel 32)
+Born 20C -> Died 20C    https://us.imdb.com/name/nm0001006/ (John Candy 43)
+Born 20C -> Died 21C    https://us.imdb.com/name/nm0670239/ (John Peel 65)
+Born 20C -> Alive       https://us.imdb.com/name/nm0088127/ (Alexis Bledel 36)
 
-Born 21C -> Died 21C    http://us.imdb.com/name/nm2548643/ (Tabea Block 1)
-Born 21C -> Alive       http://us.imdb.com/name/nm1468628/ (Ben Want 11)
+Born 21C -> Died 21C    https://us.imdb.com/name/nm2548643/ (Tabea Block 1)
+Born 21C -> Alive       https://us.imdb.com/name/nm1468628/ (Ben Want 15)
 
-Born 31 Dec 1969        http://us.imdb.com/name/nm1009503/ (Taylor McCall 44)
-Died 31 Dec 1969        http://us.imdb.com/name/nm0862239/ (Carol Thurston 46)
-Born  1 Jan 1970        http://us.imdb.com/name/nm0231191/ (Fiona Dolman 44)
-Died  1 Jan 1970        http://us.imdb.com/name/nm0902025/ (Eduard von Borsody 71)
+Born 31 Dec 1969        https://us.imdb.com/name/nm1009503/ (Taylor McCall 48)
+Died 31 Dec 1969        https://us.imdb.com/name/nm0862239/ (Carol Thurston 49)
+Born  1 Jan 1970        https://www.imdb.com/name/nm0231191/ (Fiona Dolman 48)
+Died  1 Jan 1970        https://www.imdb.com/name/nm0902025/ (Eduard von Borsody 71)
 
 http://us.imdb.com/date/{month}-{day}
 http://us.imdb.com/search/name?birth_year={year}
@@ -140,6 +130,7 @@ $
 */
 
 /*
+TODO: improve getTitleDates by grabbing month and day
 TODO: add ages to individual ages of actors to a film page, very hard,
         http req for each one, and then a xpath on the whole result
 TODO: add script updater support
@@ -148,7 +139,7 @@ TODO: add script updater support
 /*
 calculates tropical zodiac sign
 input:  month and day
-output: tropical zodiac sign as string, includes comma and label
+returns: tropical zodiac sign as string, includes comma and label
 see http://en.wikipedia.org/wiki/Signs_of_the_Zodiac
 */
 function tropicalZodiac(month, day) {
@@ -179,7 +170,7 @@ function tropicalZodiac(month, day) {
                 month ==  2 && day <= 18) {sign = "Aquarius - ♒";}
         else if(month ==  2 && day >= 19 ||
                 month ==  3 && day <= 20) {sign = "Pisces - ♓";}
-        else {return "";} /* unknown also catches odd dates */
+        else {return "";} /* unknown and also catches odd dates */
 
         /* return text with comma and label */
         return ", Tropical Zodiac Sign: " + sign;
@@ -188,7 +179,7 @@ function tropicalZodiac(month, day) {
 /*
 calculates chinese zodiac sign
 input:  full year
-output: chinese zodiac sign as string, includes comma and label
+returns: chinese zodiac sign as string, includes comma and label
 see http://en.wikipedia.org/wiki/Chinese_astrology
 */
 function chineseZodiac(year) {
@@ -214,7 +205,7 @@ function chineseZodiac(year) {
         else if (nsign ==  9) { sign = "Rooster (Earth)"; }
         else if (nsign == 10) { sign = "Dog (Metal)"; }
         else if (nsign == 11) { sign = "Pig/Wild Boar (Metal)"; }
-        else {return "";}  /* unknown also catches odd dates */
+        else {return "";}  /* unknown and also catches odd dates */
         
         /* return text with comma and label */
         return ", Chinese Zodiac Sign: " + sign;
@@ -223,12 +214,12 @@ function chineseZodiac(year) {
 /*
 get dates from a name page
 input: born and died called by ref, they are filled with dates from the page
-output: whether they are dead of alive
+returns: whether they are dead or alive
 */
 function getNameDates(born, died) {
-	var alive = false;
+				var alive = false;
 	
-        /* new style */
+        /* if new style */
         if (newStyle()) {
                 /* find the birth date */
                 var nodes = document.evaluate(
@@ -257,8 +248,8 @@ function getNameDates(born, died) {
                         died.setDate(date[2]);
                         alive = false
                 }
-        /* old style */
-	} else {
+        /* else old style */
+				} else {
                 /* find the birth date */
                 var nodes = document.evaluate(
                         "//div[contains(@class,'info-content')]/a[contains(@href,'birth')]",
@@ -304,13 +295,16 @@ function getNameDates(born, died) {
 
 /*
 get dates from a title page
+input: none
 returns: year of title
 */
 function getTitleDates() {
+
         var nodes = document.evaluate(
                 /* old style and new style */
-                "//div[contains(@id,'tn15title')]//a[contains(@href,'year')]|//*[@class='header']//span[contains(@class,'nobr')]",
+                "//div[contains(@id,'tn15title')]//a[contains(@href,'year')]|//div[@class='subtext']/a[@title='See more release dates']",
                 document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+
 
         if (nodes.snapshotLength != 1) {
                 return;
@@ -333,6 +327,7 @@ function getTitleDates() {
 /*
 add age of person to page
 input: alive status, and dates
+returns: none
 */
 function addAge(alive, born, died) {
         var justyear;
@@ -401,6 +396,7 @@ function addAge(alive, born, died) {
 /*
 add age of film and the age of the actor when they were in the film
 input: full year or birth
+returns: none
 */
 function addAges(born) {
         //find all the films, this in includes things like producer and writer
@@ -462,6 +458,7 @@ function addAges(born) {
 /*
 adds signs to page
 input: date person is born
+returns: none
 */
 function addSigns(born) {
         /* find place to stick the info */
@@ -485,6 +482,7 @@ function addSigns(born) {
 /*
 add the age of the film to the page
 input: date of film
+returns: none
 */
 function addFilmAge(filmAge) {
         /* calc age */
@@ -509,12 +507,12 @@ function addFilmAge(filmAge) {
         /* find place to stick the info */
         var nodes = document.evaluate(
                 /* old style and new style */
-                "//div[contains(@id,'tn15title')]//a[contains(@href,'year')]|//*[@class='header']//span[contains(@class,'nobr')]",
+                "//div[contains(@id,'tn15title')]//a[contains(@href,'year')]|//div[@class='subtext']/a[@title='See more release dates']",
                 document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
 
         /* create new span with formatting to match */
         var span = document.createElement('span');
-        span.style.fontSize = "15px";
+        span.style.fontSize = "11px";
         span.appendChild(container);
 
         /* should be the first occurrence of the latter */
@@ -525,6 +523,8 @@ function addFilmAge(filmAge) {
 
 /*
 find out if we are using the newstyle on not, works on name and title pages
+input: none
+returns: true if using the new style
 */
 function newStyle() {
         var nodes = document.evaluate( "//div[@id='tn15content']", document,
@@ -536,7 +536,7 @@ function newStyle() {
         }
 }
 
-/* two options, either is a name page ... */
+/* code starts, two options, either it is a name page ... */
 if (window.location.href.indexOf('name') != -1) {
         born = new Date();
         died = new Date();
@@ -554,7 +554,7 @@ if (window.location.href.indexOf('name') != -1) {
                 addAges(born.getFullYear());
         }
 
-/* ... or a title page */
+/* ... or it is a title page */
 } else if (window.location.href.indexOf('title') != -1) {
         /* get needed dates */
         filmAge = getTitleDates();
